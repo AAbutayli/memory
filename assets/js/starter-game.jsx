11a67d14a451775e1ff32063e975b2364887412c
Abdul <a.abutaily@gmail.com>
2018-10-05 15:38:17 -0400

    Second commit

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -10,51 +10,222 @@ export default function game_init(root) {
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = {
+      cardList: [],
+      score: 0,
+      openedCard: [],
+      lastOpenedIndex: -1
+    };
+    this.createCards();
+    //console.log(this.state.cardList);
   }
 
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
-  }
+  createCards() {
+    let letterList = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+    letterList = _.shuffle(letterList);
+    for (let i = 0; i < 16; i++) {
+      var card = {
+        value: '?',
+        letter: letterList[i],
+        flipped: false,
+        matched: false,
+        cardClass: 'card'
+      };
 
-  hax(_ev) {
-    alert("hax!");
-  }
-
-  render() {
-    /*let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
-
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
+      this.state.cardList.push(card);
     }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
-    }
-    */
+  }
+
+  renderCard(i) {
+
+    let card = this.state.cardList[i];
     return (
-      <div>
-        <Board />
+      <div className='col-3'>
+        <div className='card-border'>
+          <div
+            onClick={() => this.x(i)}
+            className={card.cardClass}
+            
+          >
+            {card.value}
+          </div>
+        </div>
       </div>
     );
 
+  }
 
+  renderBoard() {
+    
+    return (
+      <div>
+        <div>
+          <div className='board-row'>
+            {this.renderCard(0)}
+            {this.renderCard(1)}
+            {this.renderCard(2)}
+            {this.renderCard(3)}
+          </div>
+          <div className='board-row'>
+            {this.renderCard(4)}
+            {this.renderCard(5)}
+            {this.renderCard(6)}
+            {this.renderCard(7)}
+          </div>
+          <div className='board-row'>
+            {this.renderCard(8)}
+            {this.renderCard(9)}
+            {this.renderCard(10)}
+            {this.renderCard(11)}
+          </div>
+          <div className='board-row'>
+            {this.renderCard(12)}
+            {this.renderCard(13)}
+            {this.renderCard(14)}
+            {this.renderCard(15)}
+          </div>
+        </div>
+        <div>
+          <div className='board-row'>
+              <span>Score: </span>
+              {this.state.score}
+          </div>
+        </div>
+
+      </div>
+    );
 
   }
+
+  incrment() {
+    this.state.score++;
+  }
+
+  isFirst(){
+    return this.state.openedCard.length % 2 == 0;
+  }
+
+  flip(i){
+    var card = this.state.cardList[i]
+    if(card.flipped){
+      card.cardClass = 'card'
+      card.value = '?'
+      card.flipped = false
+      this.state.cardList[i]= card
+      this.setState({cards: this.state.cardList})
+      this.state.openedCard.splice(this.state.openedCard.length - 1, 1) 
+
+    }
+    else{
+      card.cardClass = 'flippedCard'
+      card.value = card.letter
+      card.flipped = true
+      this.state.cardList[i]= card
+      this.setState({cards: this.state.cardList})
+
+    }
+  }
+  
+  isMatched(card){
+    
+    let i = this.state.openedCard.length -1
+    let openedCard = this.state.openedCard[i]
+    if (i<0){
+      return false
+    }    
+
+    return openedCard.value == card.value;
+  }
+
+  x(i){
+    if (this.isFirst()) {
+      if (!this.state.cardList[i].matched) {
+        this.flip(i)
+        this.state.openedCard.push(this.state.cardList[i])
+        this.state.lastOpenedIndex = i
+        this.incrment()
+      }
+    }
+    else{
+      if (!this.state.cardList[i].matched && i!= this.state.lastOpenedIndex) {
+       
+        this.flip(i)
+        this.incrment()
+        if(this.isMatched(this.state.cardList[i])){
+          this.state.openedCard.push(this.state.cardList[i])
+          this.state.cardList[i].matched = true
+          this.state.cardList[this.state.lastOpenedIndex].matched = true
+          this.setState({cards: this.state.cardList})
+          this.state.lastOpenedIndex = -1
+        }else{           
+          this.flip(i) 
+          this.flip(this.state.lastOpenedIndex)
+          this.state.lastOpenedIndex = -1
+          this.state.openedCard.splice(this.state.openedCard.length - 1, 1)
+        }
+        
+      }
+    }
+  }
+
+  render(){
+
+    return (
+      <div>
+        {this.renderBoard()}
+      </div>
+    );
+  }
+
+  sleep(miliseconds) {
+    var currentTime = new Date().getTime();
+ 
+    while (currentTime + miliseconds >= new Date().getTime()) {
+    }
+ }
+
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 class Card extends React.Component {
 
   constructor(props) {
@@ -69,37 +240,51 @@ class Card extends React.Component {
   }
 
   render() {
-     
     return (
-      
-      <div className='card-border'
-      
-      >
+      <div className='card-border'>
         <div
           className={this.state.cardClass}
-          onClick={() => this.setState({ value: "letter" })}
-          >
+        /*onClick={() => this.setState({ value: "letter" })}
+        >
           {this.state.value}
-
         </div>
       </div>
     );
   }
 
-  change(){
-    if(this.state.matched){
-      return ;
+
+  flipToLetter(Card) {
+    this.state.cardClass = 'flippedCard'
+    this.state.value = this.state.letter
+    this.state.flipped = true
+    this.state.matched = Card.state.letter == this.state.letter
+
+
+  }
+
+  flipToQMark() {
+    if (!this.state.matched) {
+      this.state.cardClass = 'card'
+      this.state.value = '?'
+      this.state.flipped = false
+    }
+  }
+
+
+  change() {
+    if (this.state.matched) {
+      return;
 
     }
-    else if(!this.state.flipped){
-      this.state.cardClass= 'flippedCard'
+    else if (!this.state.flipped) {
+      this.state.cardClass = 'flippedCard'
       this.state.value = this.state.letter
-      this.state.flipped =true
+      this.state.flipped = true
     }
-    else{
-      this.state.cardClass= 'card'
+    else {
+      this.state.cardClass = 'card'
       this.state.value = '?'
-      this.state.flipped =false
+      this.state.flipped = false
     }
 
   }
@@ -164,7 +349,7 @@ class Board extends React.Component {
 
 class Score extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       count: 0,
@@ -180,8 +365,8 @@ class Score extends React.Component {
     );
   }
 
-  incrment(){
-    this.state.count += 1
+  incrment() {
+    this.state.count += 1;
 
   }
-}
+}*/
